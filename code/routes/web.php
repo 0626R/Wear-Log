cee4e08f534f26840354939b4b0f598f10dd6ec9
Rina <0626utsunomiya@gmail.com>
2025-07-29 18:36:36 +0900

    ルーティング・コントローラー新規作成

--- code/routes/web.php
+++ code/routes/web.php
@@ -2,8 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\CalendarController;
+use App\Http\Controllers\StatisticsController;
+use App\Http\Controllers\MypageController;
 
 
 // "/" にアクセスしたら、ItemController の home メソッドを呼ぶ
 Route::get('/', [ItemController::class, 'home'])->name('items.home');
 Route::get('/filter', [ItemController::class, 'home'])->name('items.filter');
+Route::get('/calendar', [CalendarController::class, 'index'])->name('calendar');
+Route::get('/statistics', [StatisticsController::class, 'index'])->name('statistics');
+Route::get('/mypage', [MypageController::class, 'index'])->name('mypage');
